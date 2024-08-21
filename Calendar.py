3c1bbbd954e88b14d900c1806490806391114d0a
tissangu <164658227+tissangu@users.noreply.github.com>
2024-08-21 00:18:38 -0500

    Add files via upload

---
+++ Calendar.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import calendar
+
+year = int(input("Enter the Year: "))
+month = int(input("Enter the Month"))
+
+print(calendar.month(year,month))
+print(calendar.calendar(year))
+
+
+# In[ ]:
+
+
+
+
